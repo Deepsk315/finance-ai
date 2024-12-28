1edfea80c00d4832f2e876e8a05287a720cfb6c7
DeepSK <deepsk3697@gmail.com>
2024-12-28 21:31:45 +0530

    updated code related to header and added new elements

--- app/page.js
+++ app/page.js
@@ -2,5 +2,9 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button>Helloooo</Button>;
+  return (
+    <div className="mt-40">
+      <Button>Helloooo</Button>
+    </div>
+  );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,26 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button";
+import Image from "next/image"
+import Link from "next/link"
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+            <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+                <Link href="/">
+                    <Image src={"/logo.png"} alt="welth logo" height={60} width={200} className="h-12 w-auto object-contain" />
+                </Link>
+                <SignedOut>
+                    <SignInButton forceRedirectUrl="/dashboard">
+                        <Button varian="outline" >Login</Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </nav>
+
+        </div>
     )
 }
 
